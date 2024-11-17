cbda0f3c90d56a05ba3bb78bff953f56dba9e88d
Dmitry Lazenyuk <lazenyukdima@gmail.com>
2024-11-17 20:39:44 +0500

    Create test sketch.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <Arduino.h>
-
+#include <FastLED.h>
+#define NUM_LEDS 60
+#define DATA_PIN 3
+CRGB leds[NUM_LEDS];
 
 void setup() {
-
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 }
 
 void loop() {
-
+  FastLED.showColor(CRGB::Blue);
 }
